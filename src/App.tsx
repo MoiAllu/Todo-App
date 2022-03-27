793d0e9bda34c582f18e140c728b86e1ada38b96
MoiAllu <al5690410@gmail.com>
2022-03-27 20:04:36 +0500

    V1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css';
+import NewTodo from './components/NewTodo';
 import Todos from './components/todos'; 
 import todo from './models/todo';
+import {useState} from 'react';
+
 function App() {
-  const todos = [
-  new todo('React Typescript'),
-  new todo('Items of typeScript')
-  ];
+  const [todos,setTodos]=useState<todo[]>([]);
+  const onAddTodoHandler=(todoText:string)=>{
+    const newtodo =new todo(todoText);
+    setTodos((prevTodo)=>{
+      return prevTodo.concat(newtodo);
+    })
+  }
   return (
     <div >
+      <NewTodo onAddTodo={onAddTodoHandler}/>
       <Todos items={todos}/>
     </div>
   );
